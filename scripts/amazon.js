1d25780acc5833688f17b8bc6abf23a88a4bd8ba
yuumi7 <alisasergeevna777@gmail.com>
2025-06-20 16:17:30 +0500

    make th quart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -75,6 +75,16 @@ document.querySelectorAll('.js-add-button')
       quantity: 1
     });
     }
-    console.log(cart);
+
+let cartQuantity = 0;
+
+cart.forEach((item) => {
+  cartQuantity += item.quantity;
+}
+);
+
+document.querySelector('.js-cart-quantity')
+.innerHTML = cartQuantity;
+
   });
 });
