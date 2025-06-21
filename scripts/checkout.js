3568c2ed9c3d9620d0f65736b6824ca2fa51e876
yuumi7 <alisasergeevna777@gmail.com>
2025-06-21 13:55:16 +0500

    fix formatprice and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {cart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
 let cartSummaryHTML = '';
 
@@ -27,7 +28,7 @@ cartSummaryHTML += `<div class="cart-item-container">
                 <div class="product-name">
                  ${matchingProduct.name}
                 </div>
-                <div class="product-price">$${matchingProduct.priceCents/100}</div>
+                <div class="product-price">$${formatCurrency(matchingProduct.priceCents)}</div>
                 <div class="product-quantity">
                   <span> Quantity: <span class="quantity-label">${cartItem.quantity}</span> </span>
                   <span class="update-quantity-link link-primary">
@@ -48,7 +49,7 @@ cartSummaryHTML += `<div class="cart-item-container">
                     type="radio"
                     checked
                     class="delivery-option-input"
-                    name="delivery-option-1"
+                    name="delivery-option-${matchingProduct.id}"
                   />
                   <div>
                     <div class="delivery-option-date">Tuesday, June 21</div>
@@ -59,7 +60,7 @@ cartSummaryHTML += `<div class="cart-item-container">
                   <input
                     type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1"
+                    name="delivery-option-${matchingProduct.id}"
                   />
                   <div>
                     <div class="delivery-option-date">Wednesday, June 15</div>
@@ -70,7 +71,7 @@ cartSummaryHTML += `<div class="cart-item-container">
                   <input
                     type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1"
+                    name="delivery-option-${matchingProduct.id}"
                   />
                   <div>
                     <div class="delivery-option-date">Monday, June 13</div>
