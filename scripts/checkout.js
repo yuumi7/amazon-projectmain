6d2f1cdeb7f74c45a71a218d988aa2089a85f9a3
yuumi7 <alisasergeevna777@gmail.com>
2025-06-23 10:22:58 +0500

    delete products from cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -34,7 +34,7 @@ cartSummaryHTML += `<div class="cart-item-container">
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id='${matchingProduct.id}'>
                     Delete
                   </span>
                 </div>
@@ -86,3 +86,11 @@ cartSummaryHTML += `<div class="cart-item-container">
 
 document.querySelector('.js-order-summary')
 .innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link')
+.forEach((link) => {
+link.addEventListener('click', () => {
+ const productId = link.dataset.productId;
+  removeFromCart(productId);
+});
+});
