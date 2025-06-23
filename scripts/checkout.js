887366eb5c6d3e2d606837dfbc8371a799dd6180
yuumi7 <alisasergeevna777@gmail.com>
2025-06-23 10:34:16 +0500

    delete products from HTML

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,8 @@ products.forEach((product) => {
 }
 });
 
-cartSummaryHTML += `<div class="cart-item-container">
+cartSummaryHTML += `<div class="cart-item-container 
+js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
             <div class="cart-item-details-grid">
@@ -92,5 +93,10 @@ document.querySelectorAll('.js-delete-link')
 link.addEventListener('click', () => {
  const productId = link.dataset.productId;
   removeFromCart(productId);
+
+ const container =  document.querySelector(
+    `.js-cart-item-container-${productId}`
+  );
+   container.remove();
 });
 });
